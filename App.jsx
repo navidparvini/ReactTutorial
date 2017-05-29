5fa1d4a41d1e6b4acb1dfabdc96851ca7d03d32a
Navid Parvini <navidparvini@navids-MacBook-Pro.local>
2017-05-29 14:01:18 +0430

    First Changes to App.jsx file

--- App.jsx
+++ App.jsx
@@ -1,11 +1,58 @@
 import React from 'react';
 
 class App extends React.Component {
+   constructor() {
+      super();
+		
+      this.state = {
+         data: 
+         [
+            {
+               "id":1,
+               "name":"Navid",
+               "age":"37"
+            },
+				
+            {
+               "id":2,
+               "name":"David",
+               "age":"30"
+            }
+         ]
+      }
+   }
+	
    render() {
       return (
          <div>
-            Hello World!!!
+            <Header/>
+            <table>
+               <tbody>
+                  {this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
+               </tbody>
+            </table>
          </div>
+      );
+   }
+}
+
+class Header extends React.Component {
+   render() {
+      return (
+         <div>
+            <h3>Name & Age</h3>
+         </div>
+      );
+   }
+}
+
+class TableRow extends React.Component {
+   render() {
+      return (
+         <tr>
+            <td>{this.props.data.name}</td>
+            <td>{this.props.data.age}</td>
+         </tr>
       );
    }
 }
